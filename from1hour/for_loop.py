9c7ebe4682e47758566756495dfe5f0447a7f91e
Nurshod <nurshodulugbekovich@gmail.com>
2022-02-12 10:00:25 +0500

    for loop

---
+++ from1hour/for_loop.py
@@ -0,0 +1,18 @@
+# 12.02.2022
+# For Loops
+
+for item in "Python":
+    print(item)
+
+for item in ["Mosh", "John", "Sarah"]:
+    print(item)
+
+for item in range(5, 10):
+    print(item)
+
+prices = [10, 20, 30]
+sum = 0
+for i in prices:
+    sum = sum + i
+
+print(f"Sum: {sum}") 
